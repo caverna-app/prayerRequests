c7b8ba04b16ec57c28759793ca2aa95a64c9c428
Rodrigo Cezar Leão <rodrigo.leao@radixeng.com.br>
2023-03-13 17:40:08 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputFormComponent } from './component/input-form/input-form.component';
 import { ReportComponent } from './component/report/report.component';
@@ -15,7 +13,6 @@ import { ReportComponent } from './component/report/report.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
